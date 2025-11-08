7993473ecffd23f2abb3391a5ce5634ebfff594c
Ugar <fubamstream@gmail.com>
2025-11-08 12:56:09 +0700

    selesai tugas satu ci4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -1,16 +1,15 @@
 <?php
 
 use CodeIgniter\Router\RouteCollection;
-use App\Controllers\KaryawanController;
-
 /**
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
 
-$routes->resource('karyawan', ['controller' => KaryawanController::class]);
-// $routes->get('karyawan', 'KaryawanController::index');
+$routes->get('/karyawan', 'KaryawanController::index');
 
-$routes->get('absensi', 'AbsensiController::index');
+// $routes->resource('karyawan', ['controller' => KaryawanController::class]);
 
-$routes->post('absensi/hadir', 'AbsensiController::tandaiHadir');
+// $routes->get('absensi', 'AbsensiController::index');
+
+// $routes->post('absensi/hadir', 'AbsensiController::tandaiHadir');
